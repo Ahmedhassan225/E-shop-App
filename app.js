51cc3829913b7560cba48d52fe723e4e45c04f8e
Ahmed Hassan <hassanahmed.ah225@gmail.com>
2021-12-18 01:03:04 +0200

    bug in add-product Fixed

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminData = require('./routes/admin');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
 
-
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
